0ebebaa3e3de67f4820b3a2c2b389d7cafd9b5c8
MarcoLopez2020 <mlopez4183@uta.edu.ec>
2024-12-26 23:42:03 -0500

    Adicion de sidenav y sections

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')  # Ruta de ejemplo
-def home():
-    return "¡Hola, mundo!"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
